78fd99ce89d59660c8c6e4f68889359492b03866
TechApus <132372486+hackathonGrupo1@users.noreply.github.com>
2023-05-05 09:09:09 -0300

    Add files via upload

---
+++ Projeto/script.js
@@ -0,0 +1,15 @@
+const button = document.getElementById("btnDrop");
+const list = document.getElementById("dropcontent");
+
+dropcontent.style.display = "none";
+
+button.addEventListener("click", (event)=> {
+  if (dropcontent.style.display == "none") {
+    dropcontent.style.display == "block"
+  } else {
+    dropcontent.style.display == "none";
+  }
+
+})
+
+
